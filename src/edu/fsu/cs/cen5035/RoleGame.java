fcc7b31571ef6cf305fcac0bef41d49b0362c8b6
Camill Folsom <cef14e@my.fsu.edu>
2019-02-02 22:57:00 -0500

    Added the SimpleHammer class
    For this commit I did the following added the SimpleHammer class,
    modified WeaponFactory to give a simple hammer with the string
    "simpleHammer", and modified RoleGame class to test simple hammer.
    In the RoleGame class I added a test function that prints the amount of
    damage a weapon does and how much damage it does dased off the armor
    value given. I test the hammer for 29 armor and 30 armor.

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -6,6 +6,13 @@ package edu.fsu.cs.cen5035;
  */
 public class RoleGame {
 
+    public static void testWeapon(Weapon weapon, String type, int armor)
+    {
+       System.out.println(type + " has " + weapon.hit() + " of damage.");
+       System.out.println(type + " was able to do " + weapon.hit(armor) + 
+                          " of damage due to an armor with "+armor+ " points.");
+    }
+
     public static void main(String[] args) {
 
     	int armor = 29;
@@ -19,6 +26,11 @@ public class RoleGame {
         System.out.println("Spear was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
         
         //TODO: Add the remainig weapons here
-
+        weapon = WeaponFactory.getWeapon("simpleHammer");
+        testWeapon(weapon, "Simple Hammer", armor);
+        armor = 30;
+        System.out.println("Simple Hammer was able to do " + weapon.hit(armor) 
+                         + " of damage due to an armor with "+ armor + 
+                           " points.");
     }
 }
