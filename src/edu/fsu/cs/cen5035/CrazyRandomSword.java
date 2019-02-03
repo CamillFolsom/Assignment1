1060a2d7d59bd8ca0bf29a0c2210ccd1f5b03a43
Camill Folsom <cef14e@my.fsu.edu>
2019-02-02 23:49:18 -0500

    Created CrazyRandomSword class
    Created the CrazyRandomSword this sword does a random number of
    hitpoints between 4 and 99. It also ignores a random amount of armor
    between 3 and one-third of the armor.

---
+++ src/edu/fsu/cs/cen5035/CrazyRandomSword.java
@@ -0,0 +1,32 @@
+package edu.fsu.cs.cen5035;
+
+/**
+ * @author: Camill Folsom
+ */
+
+public class CrazyRandomSword extends BasicWeapon implements Weapon
+{
+   /* Get a random number between 4 and 99 */
+   private static final int rand = (int) Math.random() * 99 + 4;
+
+   public CrazyRandomSword()
+   {  
+      super(rand);
+   }
+
+   @Override
+   public int hit()
+   {  return DAMAGE;}
+
+   @Override
+   public int hit(int armor)
+   {
+      /* Do a randomn amount of damge based of the armor value */
+      int damage = DAMAGE - ((int) Math.random() * (armor/3) + 3);
+
+      if(damage < 0)
+         return 0;
+
+      return damage;
+   }
+}
